cf2025d9a65cf3c14f549a61553ff8ea7c524563
Andreea Eftene <hi@pixelle.ro>
2026-02-11 00:21:25 +0100

    Improve project config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
+  reactCompiler: true,
   images: {
     deviceSizes: [600, 960, 1200, 1600, 1700, 1920],
-    loader: "custom",
-    loaderFile: "./src/lib/image-loader.ts",
+    loader: 'custom',
+    loaderFile: './src/lib/image-loader.ts',
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
