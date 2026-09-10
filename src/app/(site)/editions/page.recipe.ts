d5c98df663b1b939884ffe7e45fd1e604d05f7f4
Andreea Eftene <hi@pixelle.ro>
2026-09-11 00:38:16 +0300

    fix: increase padding for edition entry section

--- src/app/(site)/editions/page.recipe.ts
+++ src/app/(site)/editions/page.recipe.ts
@@ -4,6 +4,6 @@ export const editionsPage = sva({
   slots: ['index', 'entry'],
   base: {
     index: { listStyle: 'none', margin: '0', padding: '0' },
-    entry: { paddingBlock: 'xl', borderTop: 'hairline' },
+    entry: { paddingBlock: '3xl', borderTop: 'hairline' },
   },
 })
